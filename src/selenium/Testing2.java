b57517c7c9c5b4fb8e3fc64d3f6f243b12c025f2
panchalbhavesh5888 <panchalbhavesh5888@gmail.com>
2025-05-06 16:06:38 -0400

    Update Testing2.java
    added comment to do pull for github repo to local repo

--- src/selenium/Testing2.java
+++ src/selenium/Testing2.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 public class Testing2 {
 	//adding comment for git repo puch check
+	//adding comment again to do pull req
   @Test (groups = "impotant")
   public void test4() {
 	  System.out.println("test4");
