da63b77ed1d12e47e95606109465f93e5b12a604
Bhavesh Panchal <panchalbhavesh5888@gmail.com>
2025-05-06 15:48:19 -0400

    added comment in test2

--- src/selenium/Testing2.java
+++ src/selenium/Testing2.java
@@ -3,6 +3,7 @@ package selenium;
 import org.testng.annotations.Test;
 
 public class Testing2 {
+	//adding comment for git repo puch check
   @Test (groups = "impotant")
   public void test4() {
 	  System.out.println("test4");
