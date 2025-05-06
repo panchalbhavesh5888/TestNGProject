ce3e95f963522e40ccdfcdba5309bdb771e5278b
Bhavesh Panchal <panchalbhavesh5888@gmail.com>
2025-05-06 16:27:01 -0400

    adding code to developer_Branch

--- src/selenium/Testing2.java
+++ src/selenium/Testing2.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class Testing2 {
 	//adding comment for git repo puch check
 	//adding comment again to do pull req
+	//adding code to developer_Branch
   @Test (groups = "impotant")
   public void test4() {
 	  System.out.println("test4");
